b54b36bdcde6a3c1b34a01ee49cda8c1fe26e0d6
Ivan Paulovich <ivan.gomes@oleconsignado.onmicrosoft.com>
2018-02-28 18:06:37 -0300

    Dispatcher

---
+++ source/WebAPI/Castanha.Infrastructure/Dispatcher/Dispatcher.cs
@@ -0,0 +1,14 @@
+﻿namespace Castanha.Infrastructure.Dispatcher
+{
+    using Castanha.Application;
+    using System;
+    using Castanha.Domain;
+
+    public class Dispatcher : IDispatcher
+    {
+        public void Send(IDomainEvent domainEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
