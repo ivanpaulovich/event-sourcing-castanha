26503f8f9b99c16e48f4096966d8eb781f56062e
ivanpaulovich <ivan@100loop.com>
2018-03-03 19:48:59 -0300

    docker

--- setup/castanha.sh
+++ setup/castanha.sh
@@ -4,7 +4,7 @@ if [[ $DOCKERPULL != *"Status: Image is up to date for"* || $1 == '/f' ]]; then
         echo "Updating"
         docker stop castanha-backend
         docker rm castanha-backend
-        docker run -d -p 8040:80 -e modules__2__properties__ConnectionString=mongodb://10.0.75.1:27017 -e modules__3__properties__BrokerList=10.0.75.1:9092 --name castanha-backend ivanpaulovich/castanha:latest			
+        docker run -d -p 8040:80 -e modules__2__properties__ConnectionString=mongodb://172.17.0.1:27017 -e modules__3__properties__BrokerList=192.168.0.4:9092 --na$
 else
         echo "Image is already updated"
 fi
